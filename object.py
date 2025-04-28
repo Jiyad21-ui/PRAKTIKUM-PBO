2f904020fc7bc6580c8bdff8705ee5c0e2a1f96c
Jiyad21-ui <jiyadyad9@gmail.com>
2025-04-28 21:19:36 +0700

    Update object.py

--- object.py
+++ object.py
@@ -1,12 +1,12 @@
 class Dog:
     species = "Canine"  # Class attribute
 
-    def __init__(self, name, age):
-        self.name = name  # Instance attribute
-        self.age = age  # Instance attribute
+    def __init__(self, nim, nama):
+        self.nim = nim  # Instance attribute
+        self.nama = nama  # Instance attribute
 
 # Creating an object of the Dog class
-dog1 = Dog("Buddy", 3)
+dog1 = Dog("230705113", "jiyad")
 
-print(dog1.name) 
-print(dog1.species)
+print(dog1.nim)
+print(dog1.nama)
