62bbc2042bd346dd9e6415361c67f28948809b80
Jiyad21-ui <jiyadyad9@gmail.com>
2025-04-28 21:18:23 +0700

    Update init_method.py

--- init_method.py
+++ init_method.py
@@ -1,7 +1,12 @@
 class Dog:
-    def __init__(self, name, age):
-        self.name = name
-        self.age = age
+    species = "Canine"  # Class attribute
 
-dog1 = Dog("Buddy", 3)
-print(dog1.name) 
+    def __init__(self, nim, nama):
+        self.nim = nim  # Instance attribute
+        self.nama = nama  # Instance attribute
+
+# Creating an object of the Dog class
+dog1 = Dog("230705113", "jiyad")
+
+print(dog1.nim)
+print(dog1.nama)
